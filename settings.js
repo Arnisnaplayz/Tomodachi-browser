02157d64b19d2d64aee162d5f8d82ecb409968e3
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 02:39:28 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const backButton = document.getElementById('back');
+    
+    backButton.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+    
+ 
+});
