60392023de44c6af0b74436700f8f4f1acdcd6a1
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 02:34:26 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const playButton = document.getElementById('play');
     const settingsButton = document.getElementById('settings');
+    const backButton = document.getElementById('back'); 
     
     playButton.addEventListener('click', function() {
         window.location.href = 'game.html';
@@ -10,4 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     settingsButton.addEventListener('click', function() {
         window.location.href = 'settings.html';
     });
+    
+    
+    if (backButton) {
+        backButton.addEventListener('click', function() {
+            window.location.href = 'index.html'; 
+        });
+    }
 });
