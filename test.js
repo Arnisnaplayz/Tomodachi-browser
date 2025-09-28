3eebe64d582b3657efba84f51f06bdedd295dbca
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 02:14:39 +0300

    Update test.js
    test

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    const playButton = document.getElementById('play');
+    const settingsButton = document.getElementById('settings');
+    
+    playButton.addEventListener('click', function() {
+        // Switch to game.html
+        window.location.href = 'game.html';
+    });
+    
+    settingsButton.addEventListener('click', function() {
+        // Switch to settings.html
+        window.location.href = 'settings.html';
+    });
+});
