a96005eb510029dfefcc60740ed68a0be5537769
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 10:49:52 +0300

    Update test.js

--- main/test.js
+++ main/test.js
@@ -1,23 +1,23 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const playButton = document.getElementById('play');
     const settingsButton = document.getElementById('settings');
-    const creditsButton = document.getElementById('credits')
-    
-    playButton.addEventListener('click', function() {
-        window.location.href = 'game/index.html';
-    });
-    
-    settingsButton.addEventListener('click', function() {
-        window.location.href = 'main/settings.html';
-    });
-   
-});
-settingsButton.addEventListener('click', function() {
-        window.location.href = 'main/credits.html';
-    });
-    
+    const creditsButton = document.getElementById('credits');
+
+    if (playButton) {
+        playButton.addEventListener('click', function() {
+            window.location.href = 'game/index.html';
+        });
     }
 
-});
+    if (settingsButton) {
+        settingsButton.addEventListener('click', function() {
+            window.location.href = 'main/settings.html';
+        });
+    }
 
+    if (creditsButton) {
+        creditsButton.addEventListener('click', function() {
+            window.location.href = 'main/credits.html';
+        });
+    }
+});
