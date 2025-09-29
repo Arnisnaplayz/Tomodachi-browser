a6e9cb022542bc92e183632e8d7a23dd28e12d2f
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 17:57:15 +0300

    Update test.js

--- test.js
+++ test.js
@@ -8,38 +8,25 @@ const io = socketIo(server, {
   cors: { origin: "*" }
 });
 
-
 const onlinePlayers = new Map();
 
 io.on('connection', (socket) => {
   console.log('Player connected:', socket.id);
   
-  
   onlinePlayers.set(socket.id, {
     id: socket.id,
-    name: `Player${Math.floor(Math.random() * 1000)}`, // Random name
+    name: `Visitor${Math.floor(Math.random() * 1000)}`,
     joinedAt: new Date()
   });
   
-  
   socket.emit('onlinePlayers', Array.from(onlinePlayers.values()));
-  
-  
   socket.broadcast.emit('playerJoined', onlinePlayers.get(socket.id));
-  
-  
   io.emit('playerCount', onlinePlayers.size);
   
   socket.on('disconnect', () => {
-    const disconnectedPlayer = onlinePlayers.get(socket.id);
     onlinePlayers.delete(socket.id);
-    
-    
     io.emit('playerLeft', socket.id);
-    
-    
     io.emit('playerCount', onlinePlayers.size);
-    
     console.log('Player disconnected:', socket.id);
   });
 });
