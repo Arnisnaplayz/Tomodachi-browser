b970eb9552ef14a1eede602af166270632c30f6c
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 10:30:28 +0300

    Update settings.js

--- main/settings.js
+++ main/settings.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const backButton = document.getElementById('back');
     
     backButton.addEventListener('click', function() {
-        window.location.href = '../index.html';
+        window.location.href = 'Tomodachi-browser/index.html';
     });
     
  
