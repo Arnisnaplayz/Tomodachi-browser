47da1bb364657ee3dd3a29752817276c4c025f02
Fl1ntanywhere <89328474+Arnisnaplayz@users.noreply.github.com>
2025-09-29 17:34:29 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,30 @@
 const express = require('express');
+const http = require('http');
 const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Multiplayer server is running!');
+});
+
+io.on('connection', (socket) => {
+  console.log('Player connected:', socket.id);
+  socket.emit('connected', { message: 'Welcome!', id: socket.id });
+  
+  socket.on('disconnect', () => {
+    console.log('Player disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
